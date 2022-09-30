ac2e98b9034e3b9051cb79ee4511dbfbb7bad2ed
viniciusrodriguesb <51127512021.1@senacrjedu.local>
2022-09-30 11:27:50 -0300

    estudos Angular

---
+++ helloworld/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'helloworld';
+}
