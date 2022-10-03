88d5cb265ac32b925362d43646f09220059aaa3f
viniciusrodriguesb <51127512021.1@senacrjedu.local>
2022-10-03 11:43:00 -0300

    Estudo componentes

---
+++ Aula 06/senac/src/app/cabecalho/cabecalho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'S-cabecalho',
+  templateUrl: './cabecalho.component.html',
+  styleUrls: ['./cabecalho.component.css']
+})
+export class CabecalhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
